99bbb198b36b49482b47aa22670ac5af283f86f2
Thiago Ferezim <thiagoferezim@gmail.com>
2020-03-11 01:23:25 -0300

    add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PlansAdvantagesComponent } from './plans-advantages/plans-advantages.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '', component: HeaderComponent },
+      { path: '', component: HomeComponent },
     ])
   ],
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PlansAdvantagesComponent
+    PlansAdvantagesComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
