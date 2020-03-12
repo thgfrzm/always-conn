195b73253199ec41b885d38263ca64e0072b4fa4
Thiago Ferezim <thiagoferezim@gmail.com>
2020-03-11 21:12:31 -0300

    update signup component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.email = history.state.data.email;
+  }
+
+  onSubmit() {
+    this.router.navigate(['/plans'], { 
+      state: { 
+        data: { email: this.email, password: this.password }
+      } 
+    });
   }
 
 }
