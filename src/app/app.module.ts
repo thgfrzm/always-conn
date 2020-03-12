909881454a84bf67a224c2ba36bd99538c0bcc11
Thiago Ferezim <thiagoferezim@gmail.com>
2020-03-12 06:55:33 -0300

    fix path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,7 +12,6 @@ import { SignupComponent } from './signup/signup.component';
 import { PlansComponent } from './plans/plans.component';
 import { PlanDetailsComponent } from './plan-details/plan-details.component';
 import { UserComponent } from './user/user.component';
-import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   imports: [
