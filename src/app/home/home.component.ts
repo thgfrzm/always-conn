227ac45ba887d7b676b98023d17b3c9aa2fe33e5
Thiago Ferezim <thiagoferezim@gmail.com>
2020-03-12 06:08:26 -0300

    edit responsivity

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,14 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
   email: string;
 
-  constructor(private router: Router) { }
+  constructor(@Inject(DOCUMENT) private document: Document, private router: Router) { }
 
   ngOnInit() {
+    this.document.body.classList.add('bg-home');
   }
 
   onSubmit() {
+    this.document.body.classList.remove('bg-home');
     this.router.navigate(['/signup'], { 
       state: { 
         data: { email: this.email }
